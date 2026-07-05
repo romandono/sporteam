8845ee6fadc799e3fed5f71d4516ba95cfa89cdb
romandono <maradono_tm@hotmail.com>
2026-07-05 15:08:47 +0200

    fix: log real Prisma error in zonas controller

--- src/controllers/zona.ts
+++ src/controllers/zona.ts
@@ -1,6 +1,7 @@
 import { Response } from 'express';
 import { AuthenticatedRequest } from '../types';
 import * as zonaService from '../services/zonaService';
+import logger from '../helpers/logger';
 
 let getZonas = async (req: AuthenticatedRequest, res: Response) => {
   try {
@@ -14,8 +15,9 @@ let getZonas = async (req: AuthenticatedRequest, res: Response) => {
     }
 
     res.json({ ok: true, zonas });
-  } catch (err) {
-    res.status(400).send({ ok: false, err });
+  } catch (err: any) {
+    logger.error({ err, message: err?.message, code: err?.code, route: 'GET /zonas' }, 'Error al obtener zonas');
+    res.status(400).send({ ok: false, message: err?.message || 'No fue posible recuperar las zonas' });
   }
 };
 
